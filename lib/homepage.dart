3e992809e6283a147918e459d01225669e023521
Cocolak <krystian.figiela000@gmail.com>
2024-07-28 18:39:06 +0200

    Add Bottom Navigation Bar with functionalioty

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/profilepage.dart';
+import 'package:learn_flutter/searchPage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-        backgroundColor: const Color.fromARGB(255, 165, 42, 160),
-        //titleTextStyle: const  TextStyle(Color(Colors.white));
-      ),
-      body: ContentWidget(),
+    return const MaterialApp(
+      home: ContentWidget(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Home Page"),
     );
   }
 }
@@ -24,10 +37,50 @@ class ContentWidget extends StatefulWidget {
 }
 
 class _ContentWidgetState extends State<ContentWidget> {
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home Page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: <Widget>[
+          const Text('Home Page'),
+          const Text('Search Page'),
+          const Text('Profile Page')
+        ][currentPageIndex],
+        backgroundColor: const Color.fromARGB(255, 165, 42, 160),
+        //titleTextStyle: const  TextStyle(Color(Colors.white));
+      ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.search_outlined),
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.account_circle),
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Profile',
+          )
+        ],
+      ),
+      body: <Widget>[
+        MyHomePage(),
+        SearchPage(),
+        ProfilePage(),
+      ][currentPageIndex],
     );
   }
 }
